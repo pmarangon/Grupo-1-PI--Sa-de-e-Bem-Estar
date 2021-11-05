8ba1c3b5e9eab816486883c4bfb733512d36d46c
Gustavo Henrique Pinto <ghp2201@gmail.com>
2021-11-05 10:28:33 -0300

    Melhora a página inicial
    - Adiciona fotos no carrossel
    - Adiciona texto lateral
    - Adiciona rodapé
    - Sobreescreve cor primary para usar laranja

--- Sistema/resources/views/layouts/principal.blade.php
+++ Sistema/resources/views/layouts/principal.blade.php
@@ -1,6 +1,3 @@
-{{-- A FAZER
-  Separar estrutura HTMl em um layout
---}}
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,6 +6,13 @@
 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
+  <style type="text/css">
+    *[class*="-primary"] {
+      background-color: #ff6400 !important;
+      border-color: #ff6400 !important;
+    }
+  </style>
+
   @stack('estilos')
 
   <title>
@@ -16,11 +20,13 @@
   </title>
 </head>
 <body>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <nav class="navbar navbar-expand-lg navbar-light bg-white">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">
-        <img src="{{ asset('public/imagens/mockup_logo.jpeg') }}" alt="" width="120" height="60" class="d-inline-block align-text-top">
-      </a>
+      <div class="mx-1">
+        <a class="navbar-brand" href="#">
+          <img src="{{ asset('public/imagens/mockup_logo.jpeg') }}" alt="" width="120" height="60" class="d-inline-block align-text-top">
+        </a>
+      </div>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -35,6 +41,14 @@
   <main class="container">
     @yield('conteudo')
   </main>
+
+  <footer class="w-100 bg-primary p-3 p-md-4">
+    <div class="text-center">
+      <span class="text-white">
+        Sportzap @ {!! date('Y') !!}
+      </span>
+    </div>
+  </footer>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
--- Sistema/resources/views/tela_inicial.blade.php
+++ Sistema/resources/views/tela_inicial.blade.php
@@ -5,6 +5,10 @@
   .texto-slogan {
     place-self: center;
   }
+
+  .carousel-item img {
+    width: 85%;
+  }
 </style>
 @endpush
 
@@ -12,12 +16,12 @@
 {{-- A FAZER
   Implementar links e rotas corretas após criar a parte de autenticação
 --}}
-<li class="nav-item">
+<li class="nav-item m-1">
   <a class="btn btn-primary" href="#">
     Cadastre-se
   </a>
 </li>
-<li class="nav-item">
+<li class="nav-item m-1">
   <a class="btn btn-secondary" href="#">
     Login
   </a>
@@ -34,27 +38,57 @@
       Lorem ipsum dolor sit amet
     </h1>
   </div>
-  <div class="carousel slide" data-bs-ride="carousel">
-    <div class="carousel-inner">
-      {{-- A FAZER
-        Adicionar imagens no carrossel
-      --}}
-      <div class="carousel-item active">
-        <img src="..." class="d-block w-100">
+
+  <div class="row my-5">
+    <div class="col-12 col-sm-6 col-md-8">
+      <div class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="{{ asset('public/imagens/fotos/yoga.jpeg') }}" class="d-block mx-auto rounded-3">
+          </div>
+          <div class="carousel-item">
+            <img src="{{ asset('public/imagens/fotos/Boxe.jpg') }}" class="d-block mx-auto rounded-3">
+          </div>
+          <div class="carousel-item">
+            <img src="{{ asset('public/imagens/fotos/idosos.jpeg') }}" class="d-block mx-auto rounded-3">
+          </div>
+          <div class="carousel-item">
+            <img src="{{ asset('public/imagens/fotos/depois_do treino.jpg') }}" class="d-block mx-auto rounded-3">
+          </div>
+        </div>
       </div>
-      <div class="carousel-item">
-        <img src="..." class="d-block w-100">
-      </div>
-      <div class="carousel-item">
-        <img src="..." class="d-block w-100">
+    </div>
+    <div class="col-12 col-sm-6 col-md-4">
+      <div class="pt-4 pt-sm-0 px-4 px-sm-2">
+        <span>
+          {{-- A FAZER
+            Trocar Lorem Ipsum por texto de introdução, também estudar uma fonte maior
+          --}}
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consequat lacus at malesuada sodales. Donec bibendum sodales risus, et consequat mi suscipit sed. Mauris sed iaculis nibh. Ut sodales vestibulum sem. In hac habitasse platea dictumst. Nullam blandit maximus enim quis ultrices. Proin enim orci, molestie sed mauris eu, fringilla vulputate ante. Phasellus rhoncus pharetra auctor.
+        </span>
       </div>
     </div>
   </div>
-  {{-- A FAZER
-    Adicionar:
-    - Seção de introdução ao sistema
-    - Seção com Call To Action
-    - Rodapé
-  --}}
+
+  <div class="my-5">
+    <div class="d-grid">
+      <h2 class="texto-slogan">
+        {{-- A FAZER
+          Trocar Lorem ipsum por Call to action
+        --}}
+        Lorem ipsum dolor sit amet
+      </h2>
+    </div>
+    <div class="col-12 mt-3">
+      <div class="col-12 col-sm-4 mx-auto">
+        {{-- A FAZER
+          Implementar links e rotas corretas após criar a parte de autenticação
+        --}}
+        <a class="btn btn-primary w-100" href="#">
+          Cadastre-se
+        </a>
+      </div>
+    </div>
+  </div>
 </div>
 @endsection
