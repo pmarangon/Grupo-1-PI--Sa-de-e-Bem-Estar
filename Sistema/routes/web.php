b0eb2f28bba8d6a8b6fcee216298a0f11b346e38
Jonatas Tonin Coelho <51675838+JonatasTCoelho@users.noreply.github.com>
2021-11-06 18:16:43 -0300

    Autenticação do Usuário

--- Sistema/routes/web.php
+++ Sistema/routes/web.php
@@ -13,4 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'tela_inicial');
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
