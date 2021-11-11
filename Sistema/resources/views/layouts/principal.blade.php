03a712647d6ca6b23f3d3b704e3c94e5022671c8
Gustavo Henrique Pinto <ghp2201@gmail.com>
2021-11-11 18:37:56 -0300

    Remove footer

--- Sistema/resources/views/layouts/principal.blade.php
+++ Sistema/resources/views/layouts/principal.blade.php
@@ -54,14 +54,6 @@
     @yield('conteudo')
   </main>
 
-  <footer class="w-100 bg-primary p-3 p-md-4">
-    <div class="text-center">
-      <span class="text-white">
-        Sportzap @ {!! date('Y') !!}
-      </span>
-    </div>
-  </footer>
-
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
   @stack('scripts')
