7536674dabae56749611159689e4a93e4cd6000f
Gustavo Henrique Pinto <ghp2201@gmail.com>
2021-11-22 17:57:50 -0300

    Corrige typos

--- Sistema/resources/views/inicial/listar.blade.php
+++ Sistema/resources/views/inicial/listar.blade.php
@@ -43,7 +43,7 @@
         <div class="col-12 col-sm-6 col-md-4">
             <div class="pt-4 pt-sm-0 px-4 px-sm-2">
 		<span class="fs-4">
-                    Os esportes coletivos são muito importantes, visto que fomos obrigados a se afastar de tudo e todos, para nossa segurança durante a pandemia de Covid-19, assim já não dá mais para ficar, não é? Junte-se a equipe do SportZap e melhore suas amizades, seus momentos de lazer e sua saúde. SportZap te ajuda a encontrar pessoas próximas de você com horários condizentes ao seu e com o mesmo interesse em esportivo, quer mais? Não tem custo nenhum e você se conectará com todos por uma plataforma que já conhecida, o WhatsApp.
+                    Os esportes coletivos são muito importantes, visto que fomos obrigados a se afastar de tudo e todos, para nossa segurança durante a pandemia de Covid-19, assim já não dá mais para ficar, não é? Junte-se a equipe do SportZap e melhore suas amizades, seus momentos de lazer e sua saúde. SportZap te ajuda a encontrar pessoas próximas de você com horários condizentes ao seu e com o mesmo interesse em esportes, quer mais? Não tem custo nenhum e você se conectará com todos por uma plataforma que já conhece, o WhatsApp.
                 </span>
             </div>
         </div>
